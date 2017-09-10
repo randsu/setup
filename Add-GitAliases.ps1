48512d6d4e90735459482519851f145b97e4c98b
Richard Anderssen <trollbyte@gmail.com>
2017-09-10 23:11:04 +0200

    Use --global instead of --add
    For updating global config file.

--- Add-GitAliases.ps1
+++ Add-GitAliases.ps1
@@ -1,15 +1,15 @@
 # List existing aliases
 # git config --get-regexp git alias
 
-git config --add alias.s  status
-git config --add alias.b  branch
-git config --add alias.h  "log --all --decorate --graph --parents --source --abbrev-commit -20"
-git config --add alias.d  "diff --ignore-space-at-eol -b -w --ignore-blank-lines"
-git config --add alias.f  "fetch -a"
-git config --add alias.c  checkout
-git config --add alias.p  pull
-git config --add alias.pu push
-git config --add alias.m  merge
+git config --global alias.s  status
+git config --global alias.b  branch
+git config --global alias.h  "log --all --decorate --graph --parents --source --abbrev-commit -20"
+git config --global alias.d  "diff --ignore-space-at-eol -b -w --ignore-blank-lines"
+git config --global alias.f  "fetch -a"
+git config --global alias.c  checkout
+git config --global alias.p  pull
+git config --global alias.pu push
+git config --global alias.m  merge
 
 # Remove all aliases
 # git config --remove-section alias
