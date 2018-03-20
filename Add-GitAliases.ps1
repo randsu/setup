dc2586782362c023e8484f9856adc1c01e2d2322
Richard Sundby Anderssen <trollbyte@gmail.com>
2018-03-20 12:52:49 +0100

    Add shorthand for nice local log

--- Add-GitAliases.ps1
+++ Add-GitAliases.ps1
@@ -3,9 +3,10 @@
 
 git config --global alias.s  status
 git config --global alias.b  branch
-git config --global alias.h  "log --all --decorate --graph --parents --source --abbrev-commit -20"
-git config --global alias.d  "diff --ignore-space-at-eol -b -w --ignore-blank-lines"
-git config --global alias.f  "fetch -a"
+git config --global alias.l "log --decorate --graph --parents --source -10 --abbrev-commit"
+git config --global alias.h "log --all --decorate --graph --parents --source --abbrev-commit -20"
+git config --global alias.d "diff --ignore-space-at-eol -b -w --ignore-blank-lines"
+git config --global alias.f "fetch -a"
 git config --global alias.c  checkout
 git config --global alias.p  pull
 git config --global alias.pu push
